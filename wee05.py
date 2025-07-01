c5f010e1f9e7ace5a8321c237297e0331d5614f3
knyarku25 <knyarku@luc.edu>
2025-07-01 20:19:12 +0000

    1 Error

--- wee05.py
+++ wee05.py
@@ -1,4 +1,16 @@
-
+#This function finds the common characters betweeen two strings
+#It avoids duplicate characters in the result
+def intersection(foo: str, bar: str) -> str | None:
+    result = "" 
+    # Go through each character in the first string
+    for char in foo:
+        # If character exists in the second string and we have not added it
+        if char in bar and char not in result:
+            result += char
+    # if we don't find any common characters. return None
+    if result == "":
+        return None
+    return result        
 
 #--------------------------------------------------------------------------------#
 # ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎  WRITE YOUR CODE ABOVE THIS  LINE ⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎⬆︎
